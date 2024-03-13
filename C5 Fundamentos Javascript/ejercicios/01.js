80b85232f3c347a801baed04e5c47fe4f127f1e7
melquicedec <157919418+franquil1@users.noreply.github.com>
2024-03-12 21:00:00 -0500

    Firts commit: HTML file

--- C5 Fundamentos Javascript/ejercicios/01.js
+++ C5 Fundamentos Javascript/ejercicios/01.js
@@ -1,6 +1,6 @@
 // Crea una variable de tipo string.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoString = null;
+const nuevoString = "franquil1";
 
 module.exports = nuevoString;
